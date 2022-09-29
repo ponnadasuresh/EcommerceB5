9e98df8de72b976c32e309f59a4d673aec68dbab
suresh <ponnada.suresh@readyassist.in>
2022-09-29 15:02:53 +0530

    view product page and routing

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProductComponent } from './add-product/add-product.component';
 import { ProductsComponent } from './products.component';
+import { ViewProductComponent } from './view-product/view-product.component';
 
-const routes: Routes = [{ path: '', component: ProductsComponent }];
+// AddProductComponent,
+//     ViewProductComponent,
+//     UpdateProductComponent,
+//     DeleteProductComponent,
+//     ViewAllProductsComponent,
+//     ViewProductByDateComponent,
+//     ViewProductByCategoryComponent
+
+const routes: Routes = [
+  { path: '', component: ProductsComponent },
+  { path: 'addproduct', component: AddProductComponent },
+  { path: 'viewproduct', component: ViewProductComponent },
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
