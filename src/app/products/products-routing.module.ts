7a85f2aea3d45b3e37d781d1aff2eca2a9232328
suresh <ponnada.suresh@readyassist.in>
2022-09-29 15:45:14 +0530

    ui design for products

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddProductComponent } from './add-product/add-product.component';
 import { ProductsComponent } from './products.component';
+import { ViewAllProductsComponent } from './view-all-products/view-all-products.component';
+import { ViewProductByCategoryComponent } from './view-product-by-category/view-product-by-category.component';
+import { ViewProductByDateComponent } from './view-product-by-date/view-product-by-date.component';
 import { ViewProductComponent } from './view-product/view-product.component';
 
 // AddProductComponent,
@@ -16,6 +19,9 @@ const routes: Routes = [
   { path: '', component: ProductsComponent },
   { path: 'addproduct', component: AddProductComponent },
   { path: 'viewproduct', component: ViewProductComponent },
+  { path: 'allproducts', component: ViewAllProductsComponent },
+  { path: 'productsbydate', component: ViewProductByDateComponent },
+  { path: 'productsbycategory', component: ViewProductByCategoryComponent },
   
 
 ];
